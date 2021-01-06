56608f87560136a4ccdc553e5ded37df8b279b51
Pepe Alcácer <pepe@quddleapp.com>
2021-01-06 12:44:28 +0100

    Path option. Error with check existing file. Added Help

--- ExcelToPdf/Program.cs
+++ ExcelToPdf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -8,23 +9,75 @@ namespace ExcelToPdf
     {
         static void Main(string[] args)
         {
-            var app = new Application();
-            var rewrite = args.Any(arg => arg == "--rewrite" || arg == "-rwt");
-            var path = Directory.GetCurrentDirectory();
-            var files = Directory.GetFiles(path);
-
-            foreach (var file in files.Where(file => file.EndsWith(".xlsx")))
+            if (args.Any(arg => arg == "-h" || arg == "--help"))
             {
-                var workbook = app.Workbooks.Open(file);
-                var pdfName = file.Replace(".xlsx", ".pdf");
-
-                if (rewrite || !Directory.Exists(pdfName))
-                {
-                    workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, pdfName);
-                }
+                GetHelp();
+                return;
             }
 
-            app.Quit();
+            string path;
+            if (args.Any(arg => arg == "-p"))
+            {
+                path = GetPath();
+            }
+            else
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+
+            var rewrite = args.Any(arg => arg == "--rewrite" || arg == "-rwt");
+            Convert(path, rewrite);
+        }
+
+        private static void Convert(string path, bool rewrite)
+        {
+            var files = Directory.GetFiles(path);
+            var app = new Application();
+            try
+            {
+                foreach (var file in files.Where(file => file.EndsWith(".xlsx")))
+                {
+                    var pdfName = file.Replace(".xlsx", ".pdf");
+
+                    if (rewrite || !File.Exists(pdfName))
+                    {
+                        var workbook = app.Workbooks.Open(file);
+                        workbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF, pdfName);
+                        workbook.Close();
+                    }
+                }
+            }
+            finally
+            {
+                app.Quit();
+            }
+        }
+
+        private static string GetPath()
+        {
+            
+            {
+                Console.WriteLine("Specify path:");
+                while (true)
+                {
+                    var readedPath = Console.ReadLine();
+                    if (Directory.Exists(readedPath))
+                    {
+                        return readedPath;
+                    }
+                    Console.WriteLine("Invalid Path");
+                }
+            }
+        }
+
+        private static void GetHelp()
+        {
+            Console.Write(@"
+                This program converts the firs sheet of existing XLS in the current dir into PDF
+                -> Options:
+                    -rwt or --rewrite rewrite existing PDFs
+                    -p specify path of directory
+            ".Replace("                ", ""));
         }
     }
 }
